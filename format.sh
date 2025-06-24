b1ecf8aeb0a33993b967d318edb22ce8b00132fa
aidan <arobro@hotmail.com>
2025-06-24 14:09:58 +1200

    Formatting Script

---
+++ format.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+STYLE="{IndentWidth: 4, ColumnLimit: 200}"
+
+# Format .cpp, .h files in specific directories
+for dir in mythapps mythpluginLoader mythapps/plugins; do
+    find "$dir" -type f \( -name "*.cpp" -o -name "*.h" \) -exec clang-format -i -style="$STYLE" {} +
+done
+
+# Format .pl files
+for file in mythapps/*.pl; do
+    [ -e "$file" ] && clang-format -i "$file" -style="$STYLE"
+done
+
+# Format specific XML files
+for xml in mythapps-input mythapps-settings mythapps-ui; do
+    file="themes/default/${xml}.xml"
+    [ -e "$file" ] && xmlindent -l 10000 -nba -t -w "$file"
+done
